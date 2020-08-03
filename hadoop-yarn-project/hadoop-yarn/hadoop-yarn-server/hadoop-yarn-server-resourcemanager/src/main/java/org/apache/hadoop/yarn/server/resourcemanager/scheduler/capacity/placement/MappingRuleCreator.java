2aa8d452439a3e9725af99922445d088b8d33e22
Peter Bacsko <pbacsko@cloudera.com>
2020-08-03 16:18:14 +0200

    [WIP] YARN-10370 modify JSON schema, enhance MappingRuleCreator
    Change-Id: If4bdeb31ac2d513b092fc021f62cec89af4fa524

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/placement/MappingRuleCreator.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/placement/MappingRuleCreator.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.hadoop.yarn.server.resourcemanager.scheduler.capacity.placement;
 
 import java.io.IOException;
@@ -14,12 +32,13 @@ import org.apache.hadoop.yarn.server.resourcemanager.placement.MappingRuleMatche
 import org.apache.hadoop.yarn.server.resourcemanager.placement.MappingRuleMatchers;
 import org.apache.hadoop.yarn.server.resourcemanager.scheduler.capacity.placement.Rule.FallbackResult;
 import org.apache.hadoop.yarn.server.resourcemanager.scheduler.capacity.placement.Rule.Policy;
+import org.apache.hadoop.yarn.server.resourcemanager.scheduler.capacity.placement.Rule.Type;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MappingRuleCreator {
-  private static final String USER_TYPE = "user";
-  private static final String GROUP_TYPE = "group";
+  private static final String FULL_DEFAULT_QUEUE_PATH = "root.default";
+  private static final String DEFAULT_QUEUE = "default";
 
   public MappingRulesDescription getMappingRulesFromJson(String jsonPath)
       throws IOException {
@@ -36,48 +55,70 @@ public class MappingRuleCreator {
   public List<MappingRule> getMappingRules(String appSubmissionQueue,
       String jsonPath) throws IOException {
     MappingRulesDescription desc = getMappingRulesFromJson(jsonPath);
-    return getMappingRules(appSubmissionQueue, desc);
+    return getMappingRules(desc);
   }
 
   // TODO: main logic
   // needs some fine-tuning after things are finalized
-  List<MappingRule> getMappingRules(String appSubmissionQueue,
-      MappingRulesDescription rules) {
+  List<MappingRule> getMappingRules(MappingRulesDescription rules) {
     List<MappingRule> mappingRules = new ArrayList<>();
 
     for (Rule rule : rules.getRules()) {
       String matches = rule.getMatches();
-      String type = rule.getType();
+      Type type = rule.getType();
       Policy policy = rule.getPolicy();
       String queue = rule.getQueue();
       FallbackResult fallbackResult = rule.getFallbackResult();
+      String ruleDefaultQueue = rule.getDefaultQueue();
 
       MappingRuleMatcher matcher;
-      if (USER_TYPE.equals(type)) {
+
+      switch (type) {
+      case USER:
         matcher = MappingRuleMatchers.createUserMatcher(matches);
-      } else if (GROUP_TYPE.equals(type)) {
+        break;
+      case GROUP:
         matcher = MappingRuleMatchers.createGroupMatcher(matches);
-      } else {
-        throw new UnsupportedOperationException("User/group only right now");
+        break;
+      case APPLICATION:
+        matcher = MappingRuleMatchers.createApplicationNameMatcher(matches);
+        break;
+      default:
+        throw new IllegalArgumentException("Unknown type: " + type);
       }
 
       // action based on the policy
       MappingRuleAction action = null;
+
       switch (policy) {
       case DEFAULT_QUEUE:
-        String defaultQueue = (rule.getDefaultQueue() != null) ?
-            rule.getDefaultQueue() : "root.default";
+        String defaultQueue = getDefaultQueue(ruleDefaultQueue);
         action = MappingRuleActions.createPlaceToQueueAction(defaultQueue);
         break;
       case REJECT:
         action = MappingRuleActions.createRejectAction();
         break;
-      case RULE_PLACEMENT:
-        action = MappingRuleActions.createPlaceToQueueAction(queue);
-        break;
+      case PRIMARY_GROUP:
+        action = MappingRuleActions.createPlaceToQueueAction(
+            getTargetQueue(queue, "%primary_group"));
       case SPECIFIED_PLACEMENT:
-        action = MappingRuleActions.createPlaceToQueueAction(appSubmissionQueue);
+        action = MappingRuleActions.createPlaceToQueueAction("%specified");
         break;
+      case CUSTOM:
+        throw new UnsupportedOperationException("custom policy");
+      case NESTED_USER:
+        throw new UnsupportedOperationException("nested policy");
+      case SECONDARY_GROUP:
+        action = MappingRuleActions.createPlaceToQueueAction(
+            getTargetQueue(queue, "%secondary_group"));
+        break;
+      case USER:
+        action = MappingRuleActions.createPlaceToQueueAction(
+            getTargetQueue(queue, "%user"));
+        break;
+      default:
+        throw new UnsupportedOperationException(
+            "Unsupported policy: " + policy);
       }
 
       // fallback - what happens if the action fails?
@@ -100,5 +141,19 @@ public class MappingRuleCreator {
     }
 
     return mappingRules;
+  }
+
+  public String getDefaultQueue(String ruleDefaultQueue) {
+    if (ruleDefaultQueue == null ||
+        (FULL_DEFAULT_QUEUE_PATH.equals(ruleDefaultQueue) ||
+        DEFAULT_QUEUE.equals(ruleDefaultQueue))) {
+      return FULL_DEFAULT_QUEUE_PATH;
+    } else {
+      return ruleDefaultQueue;
+    }
+  }
+
+  public String getTargetQueue(String parent, String placeholder) {
+    return (parent== null) ? placeholder : parent + "." + placeholder;
   }
 }
