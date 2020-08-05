b7a68f6e9042e9c437cb4a562acec3ad89970de6
Gergely Pollak <shuzirra@cloudera.com>
2020-08-05 17:15:27 +0200

    Updating interface to contain the three fallback methods
    Change-Id: Ie3baecfcd78fc1e04262989eb058f8fc3d2765bb

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/placement/MappingRuleAction.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/placement/MappingRuleAction.java
@@ -16,4 +16,24 @@ public interface MappingRuleAction {
    * @return The result of the action
    */
   MappingRuleResult execute(VariableContext variables);
+
+
+  /**
+   * Sets the fallback method to reject, if the action cannot be executed the
+   * application will get rejected
+   */
+  MappingRuleActionBase setFallbackReject();
+
+  /**
+   * Sets the fallback method to skip, if the action cannot be executed
+   * We move onto the next rule, ignoring this one
+   */
+  MappingRuleActionBase setFallbackSkip();
+
+  /**
+   * Sets the fallback method to place to default, if the action cannot be executed
+   * The application will be placed into the default queue, if the default queue
+   * does not exist the application will get rejected
+   */
+  MappingRuleActionBase setFallbackDefaultPlacement();
 }
